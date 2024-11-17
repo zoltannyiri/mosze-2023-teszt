c0b4b705ac63d98f7a39d04f1b98adedc39d2914
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2024-11-17 18:14:05 +0100

    errors

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //elírás
+    std::cout << '1-100 ertekek duplazasa' //hibás szintaxis
+    for (int i = 0;) //hibás for ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //hibás for ciklus
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hibás szintaxis (nincs pontosvessző, nincs érték)
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) //hibás for ciklus
     {
-        atlag += b[i]
+        atlag += b[i] //hibás szintaxis (nincs pontosvessző)
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
